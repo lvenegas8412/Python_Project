f44f233ca042be68bbfc3510b15c15c9ea20e35d
lvenegas8412 <lvenegas8412@gmail.com>
2025-08-20 20:49:57 -0700

    Module5

--- try_except.py
+++ try_except.py
@@ -1,8 +1,44 @@
 
-#Try and Except
-astr = 'hello Bob'
-try:
-    istr = int(astr)
-except:
-    istr = -1
-print(istr)
+#TRY AND EXCEPT
+# astr = 'hello Bob'
+# try:
+#     istr = int(astr)
+# except:
+#     istr = -1
+# print(istr)
+
+
+#GRADER
+# score = float(input("Enter Score: "))
+
+# if score  <0.0 or score  >1.0:
+#     print('error, please enter a valid number')
+
+# if score >=0.9:
+#     print('A')
+# elif score >=0.8:
+#     print('B')
+# elif score >=0.7:
+#     print('C')
+# elif score >=0.6:
+#     print('D')
+# elif score <0.6:
+#     print('F')
+
+def computepay(h, r):
+    if h <= 40:
+        gross_pay = h * r
+    else:
+        overtime_hrs = h - 40
+        gross_pay = (overtime_hrs * (r * 1.5) + (40 * r))
+    return gross_pay
+
+#input hours
+hrs = float(input("Enter Hours: "))
+
+#input rate
+rate = float(input('Enter Rate: '))
+
+#func called computepay()
+p = computepay(hrs, rate )
+print("Pay", p)
