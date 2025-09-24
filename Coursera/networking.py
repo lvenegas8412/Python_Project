8a1250b338896d23f500bbe51e4b8708bc57107d
LuisV <lvenegas8412@gmail.com>
2025-09-23 21:11:01 -0700

    beautifulsoup

--- Coursera/networking.py
+++ Coursera/networking.py
@@ -1,15 +1,15 @@
-import socket
-my_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-my_sock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
-my_sock.send(cmd)
+# import socket
+# my_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# my_sock.connect(('data.pr4e.org', 80))
+# cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+# my_sock.send(cmd)
 
-while True:
-    data = my_sock.recv(512)
-    if (len(data) < 1):
-        break
-    print(data.decode())
-my_sock.close()
+# while True:
+#     data = my_sock.recv(512)
+#     if (len(data) < 1):
+#         break
+#     print(data.decode())
+# my_sock.close()
 
 # ________URL READING FROM WEB
 
@@ -18,3 +18,17 @@ my_sock.close()
 # fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.htm')
 # for line in fhand:
 #     print(line.decode().strip())
+
+# ______BEATIFUL SOUP
+
+# import urllib.request, urllib.parse, urllib.error
+# from bs4 import BeautifulSoup
+
+# url  = input('Enter: ')
+# html = urllib.request.urlopen(url).read()
+# soup = BeautifulSoup(html, 'html.parser')
+
+# tags = soup('a')
+# # for tag in tags:
+# #     print(tag.get('href', None))
+# # print(tags)
