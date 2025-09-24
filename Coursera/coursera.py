eedd05f99c549b0b679ee5f5ac356abe8486cd5c
LuisV <lvenegas8412@gmail.com>
2025-09-23 20:41:36 -0700

    urllib practice

--- Coursera/coursera.py
+++ Coursera/coursera.py
@@ -22,5 +22,12 @@
 # ##Sum up the total of the pulled numbers
 # total = sum(numlist)
 # print(total)
-    
-    
+
+# ________URL READING FROM WEB
+
+# import urllib.request, urllib.parse, urllib.error
+
+# fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.htm')
+# for line in fhand:
+#     print(line.decode().strip())
+
--- Coursera/networking.py
+++ Coursera/networking.py
@@ -10,3 +10,11 @@ while True:
         break
     print(data.decode())
 my_sock.close()
+
+# ________URL READING FROM WEB
+
+# import urllib.request, urllib.parse, urllib.error
+
+# fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.htm')
+# for line in fhand:
+#     print(line.decode().strip())
